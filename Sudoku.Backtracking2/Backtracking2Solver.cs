6b1540730f22e10379c8cd417730bcdd415af2de
Mouhamed1234ECE <129753456+Mouhamed1234ECE@users.noreply.github.com>
2024-02-23 02:52:33 +0100

    Ajout Projet Backtracking2

---
+++ Sudoku.Backtracking2/Backtracking2Solver.cs
@@ -0,0 +1,12 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.Backtracking2
+{
+    public class Backtracking2Solver : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
+        {
+            return s;
+        }
+    }
+}
