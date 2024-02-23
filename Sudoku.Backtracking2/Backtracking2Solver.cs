699fe0531233af8414ac25fde27769410a7385a5
Mouhamed1234ECE <129753456+Mouhamed1234ECE@users.noreply.github.com>
2024-02-23 15:00:57 +0100

    Ajout de la méthode conversion pour utiliser un tableau au  format int[,] au lieu de [][] imposé par le format de base

--- Sudoku.Backtracking2/Backtracking2Solver.cs
+++ Sudoku.Backtracking2/Backtracking2Solver.cs
@@ -6,7 +6,34 @@ namespace Sudoku.Backtracking2
     {
         public SudokuGrid Solve(SudokuGrid s)
         {
+            int[,] sudoku;
+
+            //Méthode pour utiliser un tableau format int[,] au lieu de [][] imposé
+            //par le format de base
+            //On créer donc un tableau int[,] qui prend toutes les valeurs de la
+            //grille de sudoku en paramètre
+            sudoku = Conversion(s);
+
+            //Boucle pour mettre à jour le tableau du suduko à retourner à partir du
+            //tableau sur lequel on a fait les modifications
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                    s.Cells[i][j] = sudoku[i, j];
+
             return s;
+        }
+        public int[,] Conversion(SudokuGrid s)
+        {
+
+            int[,] sudok = new int[10, 10];
+
+            //On remplace chaque case du nouveau tableau par la grille passée en
+            //paramètre
+            for (int i = 0; i < 9; i++)
+                for (int j = 0; j < 9; j++)
+                    sudok[i, j] = s.Cells[i][j];
+
+            return sudok;
         }
     }
 }
