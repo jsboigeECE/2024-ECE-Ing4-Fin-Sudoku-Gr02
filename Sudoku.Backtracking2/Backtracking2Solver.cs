daa55bee607ae59ff776abefe5dcc88d8fa20be8
Mouhamed1234ECE <129753456+Mouhamed1234ECE@users.noreply.github.com>
2024-02-24 22:42:38 +0100

    Ajout de la méthode IsSafe qui nous permet de vérifier la validité d'une cellule de notre grille de sudoku pour placer une valeur donnée. - D'abord on vérifie si la ligne où se situe la cellule spécifiée est valide - Ensuite on vérifie si la colonne où se situe la cellule spécifiée est valide - Enfin on vérifie si le carré 3x3 auquel appartient la cellue spécifiée est valide - Après toutes ces verifications on peut placer sûrement notre valeur dans la cellule spécifiée sans violer les règles du jeu.

--- Sudoku.Backtracking2/Backtracking2Solver.cs
+++ Sudoku.Backtracking2/Backtracking2Solver.cs
@@ -35,5 +35,24 @@ namespace Sudoku.Backtracking2
 
             return sudok;
         }
+        static bool IsSafe(int[,] grid, int row, int col, int num)
+        {
+            for (int x = 0; x <= 8; x++)
+                if (grid[row, x] == num)
+                    return false;
+
+            for (int x = 0; x <= 8; x++)
+                if (grid[x, col] == num)
+                    return false;
+
+            int startRow = row - row % 3;
+            int startCol = col - col % 3;
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    if (grid[i + startRow, j + startCol] == num)
+                        return false;
+
+            return true;
+        }
     }
 }
